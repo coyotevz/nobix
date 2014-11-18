eec41f7bac5892ff974af2cb5c54f735b617ff8b
Augusto Roccasalva <augusto@rioplomo.com.ar>
2014-11-18 13:39:34 -0300

    Rename some constraint in tax rename migration

--- migration/versions/360cddbebd21_rename_tasas_tax_table.py
+++ migration/versions/360cddbebd21_rename_tasas_tax_table.py
@@ -30,6 +30,11 @@ def upgrade():
     op.alter_column('tax', 'monto', new_column_name='amount')
     op.alter_column('tax', 'documento_id', new_column_name='document_id')
     op.add_column('tax', sa.Column('taxable', sa.Numeric(10, 2)))
+    op.drop_constraint('tasas_pkey', 'tax')
+    op.create_primary_key('tax_pkey', 'tax', ['id'])
+    op.drop_constraint('tasas_documento_id_fkey', 'tax')
+    op.create_foreign_key('tax_documento_id_fkey', 'tax', 'documentos',
+                          ['document_id'], ['id'])
 
     op.execute(
         taxhelper.update().\
@@ -37,9 +42,21 @@ def upgrade():
         )
 
     op.alter_column('tax', 'taxable', nullable=False)
+    op.create_index(op.f('ix_tax_document_id'), 'tax', ['document_id'],
+                    unique=False)
+    op.drop_index('ix_tasas_documento_id', table_name='tax')
 
 
 def downgrade():
+    op.create_index('ix_tasas_documento_id', 'tax', ['document_id'],
+                    unique=False)
+    op.drop_index(op.f('ix_tax_document_id'), table_name='tax')
+    op.drop_constraint('tax_pkey', 'tax')
+    op.create_primary_key('tasas_pkey', 'tax', ['id'])
+    op.drop_constraint('tax_documento_id_fkey', 'tax')
+    op.create_foreign_key('tasas_documento_id_fkey', 'tax', 'documentos',
+                          ['document_id'], ['id'])
+
     op.drop_column('tax', 'taxable')
     op.alter_column('tax', 'document_id', new_column_name='documento_id')
     op.alter_column('tax', 'amount', new_column_name='monto')
