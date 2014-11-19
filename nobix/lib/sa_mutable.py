7e860ce56c71a45c99da91018ccd576595f6d680
Augusto Roccasalva <augusto@rocctech.com.ar>
2014-11-19 20:36:30 -0300

    MutableList type working

--- nobix/lib/sa_mutable.py
+++ nobix/lib/sa_mutable.py
@@ -7,10 +7,11 @@
     Implements some mutable-scalar types for SQLAlchemy+PostgreSQL(psycopg2)
 """
 
+from collections import MutableSequence
 from sqlalchemy.ext.mutable import Mutable
 
 
-class MutableList(Mutable, list):
+class MutableList(Mutable, MutableSequence):
     "Use as MutableList.as_mutable(ARRAY(<type>))"
 
     @classmethod
@@ -25,17 +26,28 @@ class MutableList(Mutable, list):
         else:
             return value
 
-    def __setitem__(self, idx, value):
+    def __init__(self, initlist=None):
+        self._data = []
+        if initlist is not None:
+            self._data = list(initlist)
+
+    def __getitem__(self, index):
+        return list.__getitem__(self._data, index)
+
+    def __len__(self):
+        return list.__len__(self._data)
+
+    def __setitem__(self, index, value):
         "Detect list set events and emit change events."
-        list.__setitem__(self, idx, value)
+        list.__setitem__(self._data, index, value)
         self.changed()
 
-    def __delitem__(self, idx):
+    def __delitem__(self, index):
         "Detect list del events and emit change events."
-        list.__delitem__(self, idx)
+        list.__delitem__(self._data, index)
         self.changed()
 
-    def append(self, value):
-        "Detect list append event and emit change events."
-        list.append(self, value)
+    def insert(self, index, value):
+        "Detect list insert events and emit change events."
+        list.insert(self._data, index, value)
         self.changed()
