64c0f4bcb750b0aabb6fffe81cfb9fa931586b8c
Augusto Roccasalva <augusto@rioplomo.com.ar>
2014-11-22 10:14:53 -0300

    Adapt crypt.crypt to python2 usage

--- nobix/lib/security.py
+++ nobix/lib/security.py
@@ -3,12 +3,23 @@
     ~~~~~~~~~~~~~~~~~~
 """
 
+import string
 import crypt
 from hmac import compare_digest
+from random import SystemRandom
 
 
-def generate_password_hash(password):
-    return crypt.crypt(password)
+SALT_CHARS = string.ascii_letters + string.digits + './'
+_sr = SystemRandom()
+
+
+def mksalt():
+    return ''.join(_sr.choice(SALT_CHARS) for char in range(16))
+
+def generate_password_hash(password, salt=None):
+    if salt is None:
+        salt = mksalt()
+    return crypt.crypt(password, salt)
 
 def check_password_hash(pwhash, password):
     return compare_digest(pwhash, crypt.crypt(password, pwhash))
