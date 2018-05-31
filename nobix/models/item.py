5048c6b272e8aba59f0e9a3c90acb02d0aa4738b
Augusto Roccasalva <augusto@rioplomo.com.ar>
2018-05-31 02:35:35 -0300

    Rename item table and fields

--- nobix/models/item.py
+++ nobix/models/item.py
@@ -4,18 +4,18 @@ from . import db
 
 
 class ItemDocumento(db.Model):
-    __tablename__ = 'items_documento'
+    __tablename__ = 'item'
 
     id = db.Column(db.Integer, primary_key=True)
-    codigo = db.Column(db.UnicodeText(14))
-    descripcion = db.Column(db.UnicodeText(40), nullable=False)
-    cantidad = db.Column(db.Numeric(10, 2), nullable=False)
-    precio = db.Column(db.Numeric(10, 2), nullable=False)
+    codigo = db.Column('sku', db.UnicodeText(14))
+    descripcion = db.Column('description', db.UnicodeText(40), nullable=False)
+    cantidad = db.Column('quantity', db.Numeric(10, 2), nullable=False)
+    precio = db.Column('price', db.Numeric(10, 2), nullable=False)
 
-    articulo_id = db.Column(db.Integer, db.ForeignKey('product.id'),
+    articulo_id = db.Column('product_id', db.Integer, db.ForeignKey('product.id'),
                             index=True)
     articulo = db.relationship('Articulo', backref='doc_items')
 
-    documento_id = db.Column(db.Integer, db.ForeignKey('document.id'),
+    documento_id = db.Column('document_id', db.Integer, db.ForeignKey('document.id'),
                              index=True, nullable=False)
     documento = db.relationship('Documento', backref='items')
