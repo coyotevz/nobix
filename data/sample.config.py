0e784818cf9b18a89726fbf73f8b5c7a0981b11d
Augusto Roccasalva <augusto@rioplomo.com.ar>
2018-06-02 03:10:05 -0300

    2to3 to data/sample.config.py

--- data/sample.config.py
+++ data/sample.config.py
@@ -4,57 +4,52 @@
 
 vendedores = {
     # Ciudad
-    u'11': {'nombre': u"Carlos Roccasalva"},
-    u'21': u'11',
+    '11': {'nombre': "Carlos Roccasalva"},
+    '21': '11',
 
-    u'14': {'nombre': u"Miguel Castro"},
-    u'27': u'14',
+    '14': {'nombre': "Miguel Castro"},
+    '27': '14',
 
-    u'15': {'nombre': u"Gastón Montañes"},
-    u'20': u'15',
+    '15': {'nombre': "Gastón Montañes"},
+    '20': '15',
 
-    u'17': {'nombre': u"Carlos Moyano"},
-    u'66': u'17',
+    '17': {'nombre': "Carlos Moyano"},
+    '66': '17',
 
-    u'22': {'nombre': u"Mauricio Pereyra"},
-    u'79': u'22',
+    '22': {'nombre': "Mauricio Pereyra"},
+    '79': '22',
 
-    u'28': {
-        'nombre': u"Carmen Naselli",
-        'pass': u"",
+    '28': {
+        'nombre': "Carmen Naselli",
+        'pass': "",
     },
-    u'52': u'28',
+    '52': '28',
 
-    u'32': {'nombre': u"Daniel Lupari"},
-    u'88': u'32',
+    '32': {'nombre': "Daniel Lupari"},
+    '88': '32',
 
-    u'69': {
-        'nombre': u"Germán Roccasalva",
-        'pass': u"",
+    '69': {
+        'nombre': "Germán Roccasalva",
+        'pass': "",
     },
-    u'70': u'69',
+    '70': '69',
 
     # Godoy Cruz
-    u'13': {
-        'nombre': u"Claudia Gutierrez",
-        'pass': u"",
+    '13': {
+        'nombre': "Claudia Gutierrez",
+        'pass': "",
     },
-    u'013': u'13',
-    u'18': u'13',
+    '013': '13',
+    '18': '13',
 
-    u'12': {
-        'nombre': u"Augusto Roccasalva",
-        'pass': u"8f6febf4e159b6d35c6ac031f47c06bf",
+    '12': {
+        'nombre': "Augusto Roccasalva",
+        'pass': "8f6febf4e159b6d35c6ac031f47c06bf",
     },
-    u'19': u'12',
+    '19': '12',
 
-    u'25': {
-        'nombre': u"Jorge Museda",
+    '25': {
+        'nombre': "Jorge Museda",
     },
-    u'26': u'25',
-
-    u'33': {
-        'nombre': u"Matías Poyo",
-    },
-    u'35': u'33',
+    '26': '25',
 }
